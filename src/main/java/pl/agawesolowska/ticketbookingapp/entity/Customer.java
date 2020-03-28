7e05b79163c783aa6aa950ccde6328b6545225dd
Aga Wesolowska <a.wesolowska.1990@gmail.com>
2020-03-28 12:34:02 +0100

    validation of Polish characters

--- src/main/java/pl/agawesolowska/ticketbookingapp/entity/Customer.java
+++ src/main/java/pl/agawesolowska/ticketbookingapp/entity/Customer.java
@@ -10,10 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Pattern.Flag;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -34,25 +32,24 @@ public class Customer {
 	@Column(name = "id")
 	private Long id;
 
-	@NotBlank(message = "Please provide a first name.")
-	@Min(value = 3, message = "The first name should be at least three characters long.")
-	@Pattern(regexp = "^[A-Z][a-z]+$", flags = Flag.UNICODE_CASE, message = "The first name should start with a capital letter.")
+	@NotBlank
+	@Pattern(regexp = "^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]{2,}+$",
+		message = "The first name should be at least three characters long and start with a capital letter.")
 	@Column(name = "first_name")
 	private String firstName;
 
-	@NotBlank(message = "Please provide a last name.")
-	@Min(value = 3, message = "The last name should be at least three characters long.")
-	@Pattern(regexp = "^[A-Z][-a-zA-Z]+$", flags = Flag.UNICODE_CASE, message = "The last name should start with a capital letter"
-			+ " and could consist of two parts separated with a single dash. Keep in mind the second part should also start with a capital letter.")
+	@NotBlank
+	@Pattern(regexp = "^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]{2,}+(-[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)?$",
+		message = "The last name should be at least three characters long and start with a capital letter."
+			+ " It could also consist of two parts separated with a single dash. Keep in mind the second part should also start with a capital letter.")
 	@Column(name = "last_name")
 	private String lastName;
 
-	@NotBlank(message = "Please provide an email address.")
-	@Email(regexp = "^[\\\\w!#$%&’*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$", 
-		flags = Flag.UNICODE_CASE, message = "The email address is invalid.")
+	@NotBlank
+	@Email(regexp = "^(.+)@(.+)$", message = "The email address is invalid.")
 	@Column(name = "email_address")
 	private String emailAddress;
-	
+
 	@OneToMany(mappedBy = "customer")
 	private Set<Booking> bookings;
 
