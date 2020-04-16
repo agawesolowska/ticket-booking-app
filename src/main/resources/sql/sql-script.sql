a611a094c4645ca486130e7e225ec795258ee44f
Aga Wesolowska <a.wesolowska.1990@gmail.com>
2020-04-16 11:26:24 +0200

    added sql script

--- src/main/resources/sql/sql-script.sql
+++ src/main/resources/sql/sql-script.sql
@@ -1,15 +1,70 @@
+CREATE DATABASE ticket_booking_app;
+
+\c ticket_booking_app
+
+CREATE TABLE customer (
+id BIGSERIAL PRIMARY KEY,
+first_name VARCHAR(65) NOT NULL,
+last_name VARCHAR(65) NOT NULL,
+email_address VARCHAR(65) NOT NULL UNIQUE,
+voucher VARCHAR(15) DEFAULT NULL
+);
+
+CREATE TABLE booking (
+id BIGSERIAL PRIMARY KEY,
+creation_date_time TIMESTAMP DEFAULT NULL,
+booking_code UUID DEFAULT NULL,
+customer_id BIGINT REFERENCES customer (id)
+);
+
+CREATE TABLE cinema (
+id BIGSERIAL PRIMARY KEY,
+name VARCHAR(65) NOT NULL
+);
+
+CREATE TABLE room (
+id BIGSERIAL PRIMARY KEY,
+number INTEGER NOT NULL,
+name VARCHAR(65) NOT NULL,
+cinema_id BIGINT REFERENCES cinema (id)
+);
+
+CREATE TABLE screening (
+id BIGSERIAL PRIMARY KEY,
+screening_date_time TIMESTAMP NOT NULL,
+movie_title VARCHAR(65) NOT NULL,
+duration_time_in_minutes INTEGER NOT NULL,
+room_id BIGINT REFERENCES room (id)
+);
+
+CREATE TABLE row (
+id BIGSERIAL PRIMARY KEY,
+row_index INTEGER NOT NULL,
+number_of_seats INTEGER NOT NULL,
+screening_id BIGINT REFERENCES screening (id)
+);
+
+CREATE TABLE seat (
+id BIGSERIAL PRIMARY KEY,
+number INTEGER NOT NULL,
+is_reserved BOOLEAN DEFAULT FALSE,
+ticket_type VARCHAR(65) DEFAULT 'ADULT',
+row_id BIGINT REFERENCES row (id),
+booking_id BIGINT REFERENCES booking (id)
+);
+
 INSERT INTO cinema (name) VALUES ('Royal Cinema');
 
 INSERT INTO room (number, name, cinema_id) VALUES (1, 'Toronto', 1);
 INSERT INTO room (number, name, cinema_id) VALUES (2, 'Ontario', 1);
 INSERT INTO room (number, name, cinema_id) VALUES (3, 'Canada', 1);
 
-INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-21 15:00:00', 'Parasite', 132, 1);
-INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-21 18:00:00', 'Joker', 122, 1);
-INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-21 20:30:00', 'Ad Astra', 124, 2);
-INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-22 15:30:00', 'Green Book', 130, 2);
-INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-22 16:30:00', 'Once Upon a Time... in Hollywood', 161, 3);
-INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-22 21:00:00', 'Contagion', 105, 3);
+INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-23 15:00:00', 'Parasite', 132, 1);
+INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-23 18:00:00', 'Joker', 122, 1);
+INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-23 20:30:00', 'Ad Astra', 124, 2);
+INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-24 15:30:00', 'Green Book', 130, 2);
+INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-24 16:30:00', 'Once Upon a Time... in Hollywood', 161, 3);
+INSERT INTO screening (screening_date_time, movie_title, duration_time_in_minutes, room_id) VALUES ('2020-04-24 21:00:00', 'Contagion', 105, 3);
 
 INSERT INTO row (row_index, number_of_seats, screening_id) VALUES (1, 10, 1);
 INSERT INTO row (row_index, number_of_seats, screening_id) VALUES (2, 10, 1);
